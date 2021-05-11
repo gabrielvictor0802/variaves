318361e80d1d326bc8f3bd8e10eb75c1801c90ff
gabrielvicto0802 <gabrirlvictor0802@gmail.com>
2021-05-11 19:06:13 -0300

    variaves

---
+++ index.js
@@ -0,0 +1,9 @@
+let nome = "gabriel"
+let idade = "19" 
+let ano = 2021-19
+let souhumano = true
+let conta = 10
+let numero =9
+console.log("ola, sou o " + nome, "tenho " + idade + " anos. meu ano de nacimento é", ano, conta + numero)
+
+
